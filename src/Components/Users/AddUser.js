de6c8411ba44561dd4b5d1f49a125c1a05ace7fc
andrew jeon <andrew.jeon@box.co.kr>
2022-11-09 15:45:53 +0000

    useState 대신 ref 사용

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React,  { useState } from 'react';
+import React,  { useState, useRef} from 'react';
 import Card from '../UI/Card';
 
 import Button from '../UI/Button';
@@ -9,13 +9,16 @@ import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
 
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
-  const [error, setError] = useState();
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
@@ -35,15 +38,8 @@ const AddUser = (props) => {
 
     props.onAddUser(enteredUsername, enteredAge);
     console.log(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  }
-  const ageChangeHandler = (event) => {
-		setEnteredAge(event.target.value);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
 
@@ -66,15 +62,13 @@ const AddUser = (props) => {
 					<input
 						id='username'
 						type='text'
-						value={enteredUsername}
-						onChange={usernameChangeHandler}
+						ref={nameInputRef}
 					/>
 					<label htmlFor='age'>Age ( Years )</label>
 					<input
 						id='age'
 						type='number'
-						value={enteredAge}
-						onChange={ageChangeHandler}
+						ref={ageInputRef}
 					/>
 					<Button type='submit'>Add User</Button>
 				</form>
